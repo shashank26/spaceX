2594eaab78cc3802239efc72bb4dbea711d7013e
Shashank Sharma <shashank9226@gmail.com>
2021-03-09 01:12:14 +0530

    Trying to remove flickering

--- src/app/launch-program/launch-program.component.ts
+++ src/app/launch-program/launch-program.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as e from 'express';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { LaunchProgramModel } from '../models/launch-program.model';
 import { subjectMapKeys } from '../models/subjectMapKeys';
@@ -14,14 +16,23 @@ export class LaunchProgramComponent implements OnInit {
 
   isLoading = false;
   launchProgramData: Observable<LaunchProgramModel[]> = of([]);
+  showItems = true;
   private offset = 0;
   private limit = 10;
   private filters = {};
+  
 
-
-  constructor(private launchProgramService: LaunchProgramService, private activatedRoute: ActivatedRoute) {
+  constructor(private launchProgramService: LaunchProgramService,
+    private activatedRoute: ActivatedRoute,
+    @Inject(PLATFORM_ID) private platformId: any) {
     if (this.launchProgramService.browserType === 'MOBILE') {
       this.limit = 4;
+    }
+
+    if (isPlatformServer(this.platformId)) {
+      this.showItems = false;
+    } else {
+      this.showItems = true;
     }
     this.launchProgramData =
       this.launchProgramService.launchProgramsSubjectMap.get(subjectMapKeys.LAUNCH_PROGRAMS) as BehaviorSubject<LaunchProgramModel[]>;
