02effed236b2db148307529d60f462ca017313fc
Shashank Sharma <shashank9226@gmail.com>
2021-03-09 13:41:44 +0530

    Adding toggle functionality to filters

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-filter',
@@ -8,18 +8,32 @@ import { Router } from '@angular/router';
 })
 export class FilterComponent implements OnInit {
   public years: number[] = [];
-  public filters = {
-    launch_success: '',
-    land_success: '',
-    launch_year: ''
-  };
+  public filters: any = {};
   private currentYear = (new Date()).getUTCFullYear();
   private startYear = 2006;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private activeRoute: ActivatedRoute) {
     this.years = Array.from({ length: this.currentYear - this.startYear + 1 }, (_, i) => i + this.startYear);
   }
 
   ngOnInit(): void {
+    this.activeRoute.queryParamMap.subscribe(e => {
+      e.keys.forEach(k => {
+        this.filters[k] = this.activeRoute.snapshot.queryParamMap.get(k);
+      });
+    })
+  }
+
+  navigateTo(key: any, value: any) {
+    const val = this.filters[key];
+    if (val === String(value)) {
+      delete this.filters[key];
+    } else {
+      this.filters[key] = value;
+    }
+
+    this.router.navigate(['/launch-programs'], {
+      queryParams: this.filters,
+    });
   }
 }
--- src/app/services/converter.service.ts
+++ src/app/services/converter.service.ts
@@ -11,18 +11,21 @@ export class ConverterService {
         }
         return response.map((r: any) => {
             return {
-                image: r.image || r.links.mission_patch_small || r.links.mission_patch || r.links.flickerImages['0'],
+                image: r.image || r?.links?.mission_patch_small || r?.links?.mission_patch || r?.links?.flickerImages['0'],
                 missionName: r.missionName || r.mission_name,
                 flightNumber: r.flightNumber || r.flight_number,
                 missionIds: r.missionIds || r.mission_id,
                 launchYear: r.launchYear || r.launch_year,
                 successfulLaunch: r.successfulLaunch || r.launch_success,
-                successfulLanding: r.successfulLanding || this.wasLandingSuccessful(r.rocket.first_stage),
+                successfulLanding: r.successfulLanding || this.wasLandingSuccessful(r?.rocket?.first_stage),
             };
         });
     }
 
     private wasLandingSuccessful(rocket: any): any {
+        if (!rocket) {
+            return null;
+        }
         const success = rocket.cores.some((core: any) => core.land_success === true);
         if (success) {
             return true;
